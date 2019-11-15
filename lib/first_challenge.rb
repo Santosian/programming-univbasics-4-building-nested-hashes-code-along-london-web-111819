d02f12ac7d7c7e77f920c3402e7a18b0393be656
Santiago Molinari <santomolinari@gmail.com>
2019-11-15 22:34:26 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,7 +12,7 @@ def first_challenge
         :hero => {
           :name => "Romeo",
           :age =>"15",
-          :status = "Alive"
+          :status => "Alive"
         },
         :hero_friends => [
           {
