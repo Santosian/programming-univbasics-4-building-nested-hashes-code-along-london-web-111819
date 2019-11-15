b7dc55feb14c2a9cc5e5c99a88cf28f199b5f28b
Santiago Molinari <santomolinari@gmail.com>
2019-11-15 22:54:26 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -19,7 +19,7 @@ def fourth_challenge
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "aclive"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {
           :name => "Steven",
