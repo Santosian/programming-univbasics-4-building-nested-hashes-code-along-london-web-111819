66a27e55570f3ee5daab5bfbc97cc1294ddb9b6a
Santiago Molinari <santomolinari@gmail.com>
2019-11-15 22:14:17 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,57 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+      :montagues => {
+        :patriarch =>{
+          :name => "Lord Montagues"
+          :age => "53"
+        },
+        :matriarch => {
+          :name => "Lady Montagues"
+          :age => "54"
+        },
+        :hero => {
+          :name => "Romeo"
+          :age =>"15"
+          :status = "Alive"
+        },
+        :hero_friends => [
+          {
+          :name => "Benvolio"
+          :age => "17"
+          :attitude => "worried"
+        },
+        { :name => "Mercutio"
+          :age => "18"
+          :attitude => "hot-headed"
+        }
+      ]
+    },
+      :capulets => {
+        :patriarch => {
+          :name => "Lord Capulets"
+          :age => "50"
+        },
+        :matriarch => {
+          :name => "Lady Capulets"
+          :age => "51"
+        },
+        :heroine => {
+          :name => "Julieta"
+          :age => "15"
+          :status => "Alive"
+        },
+        :heroines_friends => [
+          {
+          :name => "Steven"
+          :age => "30"
+          :attitude => "Confused"
+          },
+          {
+          :name => "Nurse"
+          :age => "44"
+          :attitude => "Worried"
+          }
+        ]
+      }
+    }
 end
